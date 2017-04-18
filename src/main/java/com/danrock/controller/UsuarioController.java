dd4974644884c83a68bfda6b53882786cf6a10d3
Daniel Rocha <danielsantosr.rj@gmail.com>
2017-04-18 16:33:47 -0300

    desenv...

--- src/main/java/com/danrock/controller/UsuarioController.java
+++ src/main/java/com/danrock/controller/UsuarioController.java
@@ -1,32 +1,60 @@
 package com.danrock.controller;
 
-import java.util.ArrayList;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.danrock.model.Usuario;
-import com.danrock.repository.UsuarioRepository;
+import com.danrock.service.UsuarioService;
 
 @Controller
-@RequestMapping("/usuario")
 public class UsuarioController {
 	
 	@Autowired
-	private UsuarioRepository repository;
+	private UsuarioService service;
 
-	@GetMapping
-	public ModelAndView listar() {
-		List<Usuario> lista = repository.findAll();
+	@GetMapping("/usuarios")
+	public ModelAndView findAll() {
+		List<Usuario> lista = service.findAll();
 		
 		ModelAndView modelAndView = new ModelAndView("usuarios");
 		modelAndView.addObject("usuarios", lista);
 		
 		return modelAndView;
 	}
+	
+	@GetMapping("/add")
+	public ModelAndView add(Usuario usuario) {
+		
+		ModelAndView mv = new ModelAndView("/add");
+		mv.addObject("usuario", usuario);
+		
+		return mv;
+	}
+	
+	@PostMapping("/save")
+	public ModelAndView save(@Valid Usuario usuario, BindingResult result) {
+		
+		if(result.hasErrors()) {
+			return add(usuario);
+		}
+		
+		service.save(usuario);
+		
+		return findAll();
+	}
+	
+	@GetMapping("/edit/{id}")
+	public ModelAndView edit(@PathVariable("id") Long id) {
+		return add(service.findOne(id));
+	}	
 	
 }
